3721d07318530b6ac65dd5692ae1732b65db0ce4
Barney Walker <barneywalker@Jamess-MacBook-Pro-2.local>
2019-03-28 13:59:08 +0000

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='labstep',
+      version='0.1',
+      description='Python Wrapper around the Labstep API',
+      url='http://github.com/Labstep/labstepPy',
+      author='Barney Walker',
+      author_email='barney@labstep.com',
+      license='MIT',
+      packages=['labstep'],
+      zip_safe=False)
