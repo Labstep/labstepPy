133fb301233fcf0cfbfe43aae8b26b6a99fe219f
Barney Walker <barneywalker@Jamess-MacBook-Pro-2.local>
2019-04-02 09:29:58 +0100

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='labstep',
-  version='0.1',
+  version='0.1.1',
   description='Python Wrapper around the Labstep API',
   long_description=long_description,
   long_description_content_type="text/markdown",
