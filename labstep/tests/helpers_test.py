a451477b5883a0cae0f7cbac844c4276824ce166
May <May@192.168.1.3>
2019-10-14 23:38:24 +0100

    helpers_test.py and user_test.py added

--- labstep/tests/helpers_test.py
+++ labstep/tests/helpers_test.py
@@ -1,7 +1,13 @@
-from ...helpers import url_join
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('..')
+from config import API_ROOT
+from helpers import url_join
 
 
 class TestHelpers:
     def test_url_join(self):
-        assert url_join("http://api.labstep.com",
-                        "/public-api/user/login") == "http://api.labstep.com/public-api/user/login"
+        assert url_join(API_ROOT,
+                        "/public-api/user/login") == "https://api-staging.labstep.com/public-api/user/login"
--- labstep/tests/user_test.py
+++ labstep/tests/user_test.py
@@ -1,9 +1,16 @@
-from ...user import User
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-testUser = User({api-key: 'api-testing@labstep.com'})
+import labstep as LS
+
+
+testUser = LS.login('demo@labstep.com', 'demopassword')
 
 
 class TestUser:
-    def test_get_experiment():
-        exp = testUser.getExperiment(1)
-        assert exp.name == 'Name of Experiment'
+    def test_get_experiment(self):
+        exp = testUser.getExperiment(23450)
+        assert exp['name'] == "API Test Experiment", \
+            "INCORRECT EXPERIMENT NAME!"
+        assert exp['description'] == "<p>API Test Description</p>", \
+            "INCORRECT EXPERIMENT DESCRIPTION!"
