7e229a9faf6b6f03d459c788bb6235fb9e847a62
May <May@Mays-MacBook-Air.local>
2019-10-15 11:41:51 +0100

    user_test.py added test_newExperiment()

--- labstep/tests/helpers_test.py
+++ labstep/tests/helpers_test.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
-sys.path.append('..')
+sys.path.append('../')
 from config import API_ROOT
 from helpers import url_join
 
 
 class TestHelpers:
-    def test_url_join(self):
+    def test_urljoin(self):
         assert url_join(API_ROOT,
-                        "/public-api/user/login") == "https://api-staging.labstep.com/public-api/user/login"
+                        "/public-api/user/login") == "https://api-staging." \
+                        "labstep.com/public-api/user/login"
