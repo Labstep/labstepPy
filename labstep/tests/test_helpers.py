56bbd5c9b66e5dc9189d40174954705f37b833ce
May <May@Mays-MacBook-Air.local>
2019-10-16 12:12:08 +0100

    test_helpers.py added more test_functions()

--- labstep/tests/test_helpers.py
+++ labstep/tests/test_helpers.py
@@ -3,12 +3,36 @@
 # pylama:ignore=E402
 
 import sys
-sys.path.append('../')
+sys.path.append('./labstep/')
 from config import API_ROOT
-from helpers import url_join
+from helpers import (url_join, handleError, getTime, 
+                     createdAtFrom, createdAtTo, handleStatus)
+
+testDate = '2020-01-01'
 
 
 class TestHelpers:
     def test_url_join(self):
-        assert url_join(API_ROOT, "/public-api/user/login") == \
-            "https://api-staging.labstep.com/public-api/user/login"
+        assert url_join(API_ROOT, '/public-api/user/login') == \
+            'https://api.labstep.com/public-api/user/login', \
+            'FAILED TO JOIN URL'
+
+    def test_getTime(self):
+        assert len(getTime()) != 0, \
+            'FAILED TO GET TIME'
+
+    def test_createdAtFrom(self):
+        timezone = getTime()[-6:]
+        assert createdAtFrom(testDate) == \
+            testDate + 'T00:00:00{tz}'.format(tz=timezone), \
+            'FAILED TO CREATE TIME FROM'
+
+    def test_createdAtTo(self):
+        timezone = getTime()[-6:]
+        assert createdAtTo(testDate) == \
+            testDate + 'T00:00:00{tz}'.format(tz=timezone), \
+            'FAILED TO CREATE TIME TO'
+
+    def test_handleStatus(self):
+        assert handleStatus('AVaiLaBle') == 'available', \
+            'FAILED TO HANDLE STATUS'
