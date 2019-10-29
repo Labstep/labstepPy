5349041d4828aa9cf0b02dd7e4482c04d6aebd86
May <May@Mays-MacBook-Air.local>
2019-10-29 13:21:08 +0000

    added example

---
+++ example.py
@@ -0,0 +1,16 @@
+import labstep as LS
+
+# Login to your Labstep account
+user = LS.login('apitest@labstep.com', 'apitestpass')
+
+# Get a list of your experiments
+experiments = user.getExperiments(count=10)
+
+# Get a specific experiment
+my_experiment = LS.Experiment(user.getExperiment(23973), user)
+
+# Get a specific protocol
+my_protocol = user.getProtocol(4926)
+
+# Attach the protocol to the experiment
+result = my_experiment.addProtocol(my_protocol)
