95dabf20455e3817992dd54010164e998559eadd
Barney Walker <barney@labstep.com>
2019-12-18 16:51:57 +0000

    linting

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 with open("README.rst", "r") as fh:
-  long_description = fh.read()
+    long_description = fh.read()
 
 setup(name='labstep',
-  version='2.0.0',
-  description='Python SDK for working with the Labstep API',
-  long_description=long_description,
-  url='http://github.com/Labstep/labstepPy',
-  author='Barney Walker',
-  author_email='barney@labstep.com',
-  license='MIT',
-  packages=['labstep'],
-  install_requires=['requests'],
-  tests_require=["pytest"],
-  zip_safe=False)
+      version='2.0.0',
+      description='Python SDK for working with the Labstep API',
+      long_description=long_description,
+      url='http://github.com/Labstep/labstepPy',
+      author='Barney Walker',
+      author_email='barney@labstep.com',
+      license='MIT',
+      packages=['labstep'],
+      install_requires=['requests'],
+      tests_require=["pytest"],
+      zip_safe=False)
