b6c640bd03db6364ce5697137a5d043bf3337548
Barney Walker <barney@labstep.com>
2020-01-06 15:39:15 +0000

    dotenv

--- labstep/config.py
+++ labstep/config.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-API_ROOT = 'https://api.labstep.com/'
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API = os.getenv('LABSTEP_API')
+
+if API == 'testing':
+    API_ROOT = 'https://api-testing.labstep.com/'
+elif API == 'staging':
+    API_ROOT = 'https://api-staging.labstep.com/'
+else:
+    API_ROOT = 'https://api.labstep.com/'
