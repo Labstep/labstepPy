d209e778ad79cc139d20e2a7a0599b649a5d3aac
Barney Walker <barney@labstep.com>
2020-01-06 15:41:23 +0000

    version num

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(name='labstep',
-      version='2.0.3',
+      version='2.0.2',
       description='Python SDK for working with the Labstep API',
       long_description=long_description,
       url='http://github.com/Labstep/labstepPy',
