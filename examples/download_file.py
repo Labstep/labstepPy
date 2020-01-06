bdae98f46fe8ce79c75053d683b638740ad30727
Barney Walker <barney@labstep.com>
2020-01-06 14:05:29 +0000

    save method

--- examples/download_file.py
+++ examples/download_file.py
@@ -15,10 +15,9 @@ workspace_files = my_workspace.getFiles()
 rawData = my_file.getData()
 
 # Or save directly as a new file
-my_file.saveAs('./new_file.png')
+my_file.save()
 
 # If you come across a file_id attached to another labstep entity
 # you can retrieve the file using the User getFile method
-
 my_file = user.getFile(123)
-my_file.saveAs('./attached_file.txt')
+my_file.save()
