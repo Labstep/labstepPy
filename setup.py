9db679e38874fe26e0da2e4f48a4f2ce88987f0d
Jan Domanski <jandom@gmail.com>
2020-05-15 13:43:45 +0100

    Refactor a bit

--- setup.py
+++ setup.py
@@ -8,21 +8,6 @@ with open("README.rst", "r") as fh:
 
 VERSION = '2.3.1'
 
-
-class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
-
-    def run(self):
-        tag = os.getenv('CIRCLE_TAG')
-
-        if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(
-                tag, VERSION
-            )
-            sys.exit(info)
-
-
 setup(name='labstep',
       version=VERSION,
       description='Python SDK for working with the Labstep API',
