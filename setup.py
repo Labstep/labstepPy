f23d9c77e7bcff3c5aa50e0b8690e3a8a5e5b9c0
Barney Walker <barney@labstep.com>
2020-05-15 17:36:19 +0100

    version single source

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
-from labstep.version import version
+
+version = {}
+
+with open("labstep/version.py") as fp:
+    exec(fp.read().strip(), version)
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(name='labstep',
-      version=version,
+      version=version['version'],
       description='Python SDK for working with the Labstep API',
       long_description=long_description,
       url='http://github.com/Labstep/labstepPy',
