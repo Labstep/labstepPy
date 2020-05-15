57463403840e94466424657ddf0d2cabc95b12d9
Jan Domanski <jandom@gmail.com>
2020-05-15 12:13:15 +0100

    Test deploy to testpypi

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
+import os
+import sys
 from setuptools import setup
+from setuptools.command.install import install
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+VERSION = '2.3.1'
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
 setup(name='labstep',
-      version='2.3.1',
+      version=VERSION,
       description='Python SDK for working with the Labstep API',
       long_description=long_description,
       url='http://github.com/Labstep/labstepPy',
